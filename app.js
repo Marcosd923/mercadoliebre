c2b412a15c7defd6a70c85fc6a78335b9cd6e03c
Marcos923 <marcos.daut@gmail.com>
2022-12-14 21:11:09 -0300

    agregando nmp start

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
-app.listen(3000, () => {
-  console.log("el servidor anda bien");
+let port = process.env.port || 3000;
+
+app.listen(port, () => {
+  console.log("el servidor anda bien" + port);
 });
 
 app.use(express.static("public"));
